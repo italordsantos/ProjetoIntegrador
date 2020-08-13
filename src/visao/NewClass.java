65a96ce95e94447a688041af67dc3deed037e386
Ítalo Rodrigues <italo.rgrego@gmail.com>
2020-08-12 22:05:38 -0300

    idealizando estrutura de vacina

---
+++ src/visao/NewClass.java
@@ -0,0 +1,98 @@
+/*
+
+
+
+
+
+
+
+VACINA   ANIMAL   DATA APLICADO   DATA RETORNO    STATUS
+ V10      BILI     02/08/2020      05/08/2020      REALIZADO
+ V10      BILI     05/08/2020      07/09/2020      REALIZADO
+ V10      BILI     15/09/2020      07/10/2020      REALIZADO COM ATRASO
+
+REALIZADO
+REALIZADO COM ATRASO
+CANCELADO
+ATRASADO
+
+hoje -> 09/08/2020
+
+
+
+
+
+
+
+
+
+ V10      BILI     12/09/2020      12/10/2020
+ V10      BILI     12/10/2020      12/10/2021
+                                   
+
+
+1 MÊS  --  1 ANO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
